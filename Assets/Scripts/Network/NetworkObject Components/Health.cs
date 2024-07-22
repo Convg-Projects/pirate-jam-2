b98d3c888a01a4ad78f6e933623549eb39ef0804
Convg-Projects <Convg.Code@gmail.com>
2024-07-22 17:26:20 +0100

    health script updated for display

--- Assets/Scripts/Network/NetworkObject Components/Health.cs
+++ Assets/Scripts/Network/NetworkObject Components/Health.cs
@@ -2,23 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
 public class Health : NetworkBehaviour
 {
   [SerializeField]private int maxHealth = 100;
   [SerializeField]private bool destroyOnDeath = false;
-  [SerializeField]private bool isPlayer;
+  [SerializeField]private bool isPlayer = false;
+  [SerializeField]private bool displayHealth = false;
+  [SerializeField]private TextMeshProUGUI healthText;
   NetworkVariable<int> health = new NetworkVariable<int>();
   public NetworkVariable<bool> dead = new NetworkVariable<bool>();
 
   public override void OnNetworkSpawn(){
     ChangeHealthServerRpc(maxHealth);
     health.OnValueChanged += OnHealthChanged;
+    if(displayHealth){
+      healthText.text = maxHealth;
+    }
 
     base.OnNetworkSpawn();
   }
 
   public void OnHealthChanged(int previous, int current){
+    if(displayHealth){
+      healthText.Text = health;
+    }
     if(current <= 0 && !dead.Value){
       HandleDeathRpc();
     }
